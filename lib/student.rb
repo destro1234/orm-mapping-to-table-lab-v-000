df7e2f68cd78e1532d0b3bd5193b2ac16795e8e4
Elias Ventura <eliasventura23@gmail.com>
2019-01-18 21:02:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,15 +9,6 @@ class Student
   end
 
   def self.create_table
-    sql = <<-SQL
-    CREATE TABLE (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER
-    )
-    SQL
-
-    DB[:conn].execute(sql)
 
   end
 

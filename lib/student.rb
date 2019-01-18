26d0656bfaf316cf8999a2d530ee7b1d19000243
Elias Ventura <eliasventura23@gmail.com>
2019-01-18 21:01:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS (
+    CREATE TABLE (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER

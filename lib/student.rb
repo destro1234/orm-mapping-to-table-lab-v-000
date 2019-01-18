a2f710ee5820ac185dc2265160c5b3810bced2e4
Elias Ventura <eliasventura23@gmail.com>
2019-01-18 20:59:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,4 +8,17 @@ class Student
     @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+
+  end
+
 end
